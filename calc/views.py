dead3ff288b125f358a13f6a47d7c04b99863fd5
rcruiz <rc.ruiz@alumnos.urjc.es>
2016-03-28 23:31:26 +0200

    Calculadora DJANGO hecha

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+
 
 # Create your views here.
+#def say_main(request):
+#    return HttpResponse('<h1>Calculadora Django</h1>')
+def suma(request, num1, suma, num2):
+    return HttpResponse('La suma es ' + str(int(num1) + int(num2)))
+
+
+def resta(request, num1, resta, num2):
+    return HttpResponse('La resta es ' + str(int(num1) - int(num2)))
+
+
+def multiplica(request, num1, mult, num2):
+    return HttpResponse('El producto es ' + str(int(num1) * int(num2)))
+
+
+def divide(request, num1, div, num2):
+    try:
+        return HttpResponse('El cociente es ' + str(int(num1) / int(num2)))
+    except ZeroDivisionError:
+        return HttpResponse('No se puede dividir entre 0')
+
+
+def error(request):
+    return HttpResponseNotFound('Error 404 :Recursos introducidos invalidos')
--- project/urls.py
+++ project/urls.py
@@ -5,6 +5,11 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    # url(r'^$', 'calc.views.say_main',),
+    url(r'^(\d+)(\+)(\d+)$', 'calc.views.suma',),
+    url(r'^(\d+)(\-)(\d+)$', 'calc.views.resta',),
+    url(r'^(\d+)(\*)(\d+)$', 'calc.views.multiplica',),
+    url(r'^(\d+)(\/)(\d+)$', 'calc.views.divide',),
+    url(r'^.*$', 'calc.views.error',),
     url(r'^admin/', include(admin.site.urls)),
 )
